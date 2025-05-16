5f770ac55e54e9e9c2e8d5c5f79b1b20925f33f4
MarkRegieRamos <markramosregie19@gmail.com>
2025-05-16 20:45:12 +0800

    update

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -3,7 +3,7 @@ from scholarship_checker import is_eligible_for_scholarship
 
 
 class TestScholarshipEligibility(unittest.TestCase):
-     
+  
     def test_eligible_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
@@ -12,10 +12,10 @@ class TestScholarshipEligibility(unittest.TestCase):
 
     def test_high_income(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 30000))
-        
+
     def test_low_gpa_high_income(self):
         self.assertTrue(is_eligible_for_scholarship(3.2, 35000))
- 
+
     def test_exact_threshold(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
