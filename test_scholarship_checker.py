4e289b143918343db4da1e2c598fbcbd7eae05e6
yasmienmbanal <yasmienbanal0@gmail.com>
2025-05-16 20:25:24 +0800

    update

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -1,8 +1,8 @@
 import unittest
 from scholarship_checker import is_eligible_for_scholarship
 
-class TestScholarshipEligibility(unittest.TestCase):
 
+class TestScholarshipEligibility(unittest.TestCase):
      def test_eligible_student(self):
           self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
@@ -23,6 +23,7 @@ class TestScholarshipEligibility(unittest.TestCase):
 
      def test_negative_income(self):
           self.assertTrue(is_eligible_for_scholarship(3.8, -1000))
+
 
      if __name__ == "__main__":
           unittest.main()
