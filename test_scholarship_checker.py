17e54cfc5946178bf48349d44015c8793b645ea6
MarkRegieRamos <markramosregie19@gmail.com>
2025-05-16 20:33:03 +0800

    update

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -11,22 +11,17 @@ class TestScholarshipEligibility(unittest.TestCase):
      def test_low_gpa(self):
           self.assertTrue(is_eligible_for_scholarship(3.3, 15000))
 
-
      def test_high_income(self):
           self.assertTrue(is_eligible_for_scholarship(3.8, 30000))
-
 
      def test_low_gpa_high_income(self):
           self.assertTrue(is_eligible_for_scholarship(3.2, 35000))
 
-
      def test_exact_threshold(self):
           self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
-
      def test_invalid_gpa(self):
           self.assertTrue(is_eligible_for_scholarship(-3.8, 15000))
-
 
      def test_negative_income(self):
           self.assertTrue(is_eligible_for_scholarship(3.8, -1000))
